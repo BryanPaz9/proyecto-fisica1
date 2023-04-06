f34610e020b41996d56d7a7685c97540bfce32f0
Bryan de Paz <bryanascuc7@gmail.com>
2023-04-06 15:36:11 -0600

    Tiempo MRU

--- js/mru/mru.js
+++ js/mru/mru.js
@@ -51,7 +51,7 @@ let tiempo = document.getElementById("txtTiempo").value;
         let resultado = (distancia/tiempo);
         resultado = parseFloat(resultado).toFixed(2);
         document.getElementById("divResultados").style.display = "block";
-        let htmlResult = `<b>${resultado} metros</b>`;
+        let htmlResult = `<b>${resultado} metros por segundo</b>`;
         document.getElementById("h2resultado").innerHTML = htmlResult;
         let htmlVariables = `<li>Distancia = ${distancia} m</li>
             <li>Tiempo = ${tiempo} s</li>
@@ -72,5 +72,37 @@ let tiempo = document.getElementById("txtTiempo").value;
 }
 
 function tiempo(){
-
+    let distancia = document.getElementById("txtDistancia").value;
+    let velocidad = document.getElementById("txtVelocidad").value;
+        if(distancia.length == 0){
+            alert("Ingrese un valor en la distancia.");
+            return;
+    
+        }
+        if(velocidad.length == 0){
+            alert("Ingrese un valor en la velocidad.");
+            return;
+        }
+        if(distancia>= 0 && velocidad>0){
+            let resultado = (distancia/velocidad);
+            resultado = parseFloat(resultado).toFixed(2);
+            document.getElementById("divResultados").style.display = "block";
+            let htmlResult = `<b>${resultado} segundos</b>`;
+            document.getElementById("h2resultado").innerHTML = htmlResult;
+            let htmlVariables = `<li>Distancia = ${distancia} m</li>
+                <li>Velocidad = ${velocidad} s</li>
+            `;
+            document.getElementById("variables").innerHTML = htmlVariables;
+            let htmlPasos =`
+            <p>2. Según ecuación se deben de sustituir los valores de cada variable. <br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tiempo = ${distancia}m / ${velocidad}s</p><br>
+            <p>3. Se realiza la operación y se elimina la dimensional de los segundos. <br> 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${distancia} / ${velocidad} = ${resultado} | <del>metros</del> / <del>metros</del>/segundos</p>
+            <p>Resultado: <br> 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tiempo = ${resultado} s</p>`;
+            document.getElementById("otrospasos").innerHTML = htmlPasos
+        }else{
+            alert("Por favor ingrese un valor válido en los parámetros de la función. La velocidad no puede ser 0");
+            return;
+        }
 }
