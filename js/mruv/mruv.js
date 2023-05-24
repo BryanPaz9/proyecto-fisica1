319840e911c06a4d86347f98a0b66ac9be3e2e2c
Bryan de Paz <bryanascuc7@gmail.com>
2023-05-23 23:01:15 -0600

    Agregando velocidad media

--- js/mruv/mruv.js
+++ js/mruv/mruv.js
@@ -99,10 +99,11 @@ let velocidad_final = document.getElementById("txtVelocidadFinal").value;
         document.getElementById("variables").innerHTML = htmlVariables;
         let htmlPasos =`
         <p>2. Encontrar una fórmula que cumpla con las variables que tenemos a disposición.</p>
-        <p>Utilizaremos la fórmula para encontrar la distancia.</p>
-        <p><ul><li>d = vo*t + 1/2*a*t^2</li></ul> </p>
+        <p>Utilizaremos la fórmula de Velocidad final al cuadrado, pero debemos realizar un despeje.</p>
+        <p><ul><li>Vf^2 = Vo^2 + 2ad</li></ul> </p>
         <p>3. Se despeja la velocidad inicial y la ecuación queda así. </p>
-        <p> Vo = (d-1/2at^2)t</p>
+        <p> Vo = sqrt(Vf^2-2ad)</p>
+        <small>sqrt = raíz cuadrada</small><br>
         <p>4. Se realiza la operación y se eliminan las dimensionales para dejar solamente metros sobre segundos. <br> 
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Velocidad Inicial = sqrt((${velocidad_final} m/s)^2 - (2 * ${aceleracion} m/s^2 * ${distancia} m))</p><br>
         <p>Resultado: <br> 
@@ -139,12 +140,14 @@ let velocidad_final = document.getElementById("txtVelocidadFinal").value;
         `;
         document.getElementById("variables").innerHTML = htmlVariables;
         let htmlPasos =`
-        <p>2. Encontrar una fórmula que cumpla con las variables que tenemos a disposición.</p>
-        <p><ul><li>Vo = Vf - a*t</li></ul> </p>
-        <p>3. Se realiza la operación y se eliminan las dimensionales para dejar solamente metros sobre segundos. <br> 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Velocidad Inicial = ${velocidad_final} m/s - (${aceleracion} m/s^2 * ${tiempo} s))</p><br>
+        <p>2. Encontrar una fórmula que cumpla con las variables que tenemos a disposición. En este caso utilizaremos la fórmula de la distancia y despejaremos la velocidad inicial.</p>
+        <p><ul><li>d = <b>Vo</b>*t + 1/2*a*t^2</li></ul> </p>
+        <p>3. Se realiza el respectivo despeje y la fórmula nos queda de la siguiente manera.</p>
+        <p><ul><li><b>Vo</b> = (d - 1/2*a*t^2)/t </li></ul> </p>
+        <p>4. Se realiza la operación y se eliminan las dimensionales para dejar solamente metros sobre segundos. <br> 
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>Velocidad Inicial</b> = (${distancia} - 1/2*${aceleracion}*${tiempo}^2)/${tiempo} m/s</p><br>
         <p>Resultado: <br> 
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Velocidad Inicial = ${velocidad_inicial} m/s</p>`;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Velocidad Inicial</b> = ${velocidad_inicial} m/s</p>`;
         document.getElementById("otrospasos").innerHTML = htmlPasos;
     }else{
         alert("Por favor ingrese un valor válido en los parámetros de la función.");
@@ -158,21 +161,21 @@ async function vo_sin_t(velocidad_final,aceleracion,distancia){
     let d = parseInt(distancia);
     let vo = Math.sqrt((Math.pow(vf,2)-(2*a*d)));
     return vo;
-  }
+}
   async function vo_sin_d(velocidad_final,aceleracion,tiempo){
     let vf = parseInt(velocidad_final);
     let a = parseInt(aceleracion);
     let t = parseInt(tiempo);
     let vo = (vf-(a*t));
     return vo;
-  }
+}
   async function vo_sin_vf(aceleracion,distancia,tiempo){
     let a = parseInt(aceleracion);
     let d = parseInt(distancia);
     let t = parseInt(tiempo);
     let vo = ((d-((a * Math.pow(t,2))/2))/t);
     return vo;
-  }
+}
 
 /**
  * FUNCIONES DE VELOCIDAD FINAL
@@ -242,20 +245,65 @@ async function vf_sin_t(velocidad_inicial,distancia,aceleracion){
     let d = parseInt(distancia);
     let vf = Math.sqrt((Math.pow(vo,2)+2*(a*d)));
     return vf;
-  }
+}
   
-  async function vf_sin_d(velocidad_inicial,aceleracion,tiempo){
+async function vf_sin_d(velocidad_inicial,aceleracion,tiempo){
     let vo = parseInt(velocidad_inicial);
     let a = parseInt(aceleracion);
     let t = parseInt(tiempo);
     let vf = vo+(a*t);
     return vf;
-  }
+}
 
 /**
  * FUNCIONES DE TIEMPO
  * @returns 
  */
+/**
+ * FUNCIONES DE VELOCIDAD MEDIA
+ */
+
+async function velocidad_media(){
+    let distancia_final = parseFloat(document.getElementById('txtDistanciaFinal').value);
+    let distancia_inicial = parseFloat(document.getElementById('txtDistanciaInicial').value);
+    let tiempo_final = parseFloat(document.getElementById('txtTiempoFinal').value);
+    let tiempo_inicial = parseFloat(document.getElementById('txtTiempoInicial').value);
+    if(tiempo_final == tiempo_inicial){
+        alert("Los tiempos no pueden ser los mismos, ni el denominador puede ser 0");
+        return;
+    }else if(distancia_final<distancia_inicial){
+        alert("Las distancia final no puede ser menor que la distancia inicial");
+        console.log(distancia_final,distancia_inicial);
+        return;
+    }else{
+        let velocidad_media = await vm(distancia_final,distancia_inicial,tiempo_final,tiempo_inicial);
+        document.getElementById("divResultados").style.display = "block";
+        let htmlResult = `<b>${velocidad_media} metros por segundo</b>`;
+        document.getElementById("h2resultado").innerHTML = htmlResult;
+        let htmlVariables = `<li>Distancia final = ${distancia_final} m</li>
+            <li>Distancia inicial = ${distancia_inicial} m</li>
+            <li>Tiempo final = ${tiempo_final} s</li>
+            <li>Tiempo inicial = ${tiempo_inicial} s</li>
+        `;
+        document.getElementById("variables").innerHTML = htmlVariables;
+        let htmlPasos =`
+        <p>Utilizaremos la fórmula de la Velocidad Media.</p>
+        <p>2. Se realiza la operación y se eliminan las dimensionales para dejar solamente metros sobre segundos. <br> 
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Velocidad Media = (${distancia_final} m - ${distancia_inicial} m)/(${tiempo_final} s - ${tiempo_inicial} s) </p><br>
+        <p>Resultado: <br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Velocidad Media = ${velocidad_media} m/s</p>`;
+        document.getElementById("otrospasos").innerHTML = htmlPasos;
+    }
+}
+
+async function vm(distancia_final,distancia_inicial,tiempo_final,tiempo_inicial){
+    let xf = parseFloat(distancia_final);
+    let xo = parseFloat(distancia_inicial);
+    let tf = parseFloat(tiempo_final);
+    let to = parseFloat(tiempo_inicial);
+    let vm = ((xf-xo)/(tf-to)).toFixed(2);
+    return vm;
+}
 
 async function tiempo(){
     let distancia = document.getElementById("txtDistancia").value;
